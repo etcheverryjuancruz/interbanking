78c86e26f3ada50e6064aac78c0b7bbbe1577298
jetcheverry <etcheverryjuancruz@gmail.com>
2025-02-28 17:46:21 -0300

    Add swagger configuration and controller documentation

--- src/main/java/com/jetcheverry/interbanking/infrastructure/api/controller/CompanyController.java
+++ src/main/java/com/jetcheverry/interbanking/infrastructure/api/controller/CompanyController.java
@@ -7,11 +7,16 @@ import com.jetcheverry.interbanking.domain.port.in.TransferServicePort;
 import com.jetcheverry.interbanking.infrastructure.api.dto.CompanyRequestDto;
 import com.jetcheverry.interbanking.infrastructure.api.dto.CompanyResponseDto;
 import com.jetcheverry.interbanking.infrastructure.api.mapper.CompanyDtoMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +25,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/companies")
+@Tag(name = "Company Controller", description = "Endpoints para gestionar empresas")
 public class CompanyController {
 
     private final CompanyServicePort companyService;
@@ -39,6 +45,17 @@ public class CompanyController {
         );
     }
 
+    @Operation(
+            summary = "Registrar una nueva empresa",
+            description = "Registra una empresa en el sistema. Si no se proporciona la fecha de adhesión (`joinDate`), se asignará la fecha actual.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Empresa registrada exitosamente",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CompanyResponseDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Solicitud inválida"),
+                    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+            }
+    )
     @PostMapping
     public ResponseEntity<CompanyResponseDto> registerCompany(@Valid @RequestBody CompanyRequestDto companyRequestDto) {
         var savedCompany = companyService.registerCompany(companyDtoMapper.toDomain(companyRequestDto));
@@ -46,8 +63,22 @@ public class CompanyController {
     }
 
 
+    @Operation(
+            summary = "Obtener empresas filtradas",
+            description = "Obtiene una lista de empresas basadas en el filtro seleccionado. Los filtros disponibles son: " +
+                    "`JOINED_LAST_MONTH` (Empresas adheridas el último mes) y `WITH_TRANSFERS_LAST_MONTH` " +
+                    "(Empresas que realizaron transferencias en el último mes).",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Lista de empresas obtenida exitosamente",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CompanyResponseDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Filtro inválido"),
+                    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+            }
+    )
     @GetMapping
     public ResponseEntity<List<CompanyResponseDto>> getCompanies(
+            @Parameter(description = "Tipo de filtro para obtener empresas", example = "JOINED_LAST_MONTH")
             @RequestParam("filter") CompanyFilterType filterType) {
 
         List<Company> companies = filterHandlers.getOrDefault(filterType, Collections::emptyList).get();
--- src/main/java/com/jetcheverry/interbanking/infrastructure/api/controller/TransferController.java
+++ src/main/java/com/jetcheverry/interbanking/infrastructure/api/controller/TransferController.java
@@ -5,6 +5,12 @@ import com.jetcheverry.interbanking.domain.port.in.TransferServicePort;
 import com.jetcheverry.interbanking.infrastructure.api.dto.TransferRequestDto;
 import com.jetcheverry.interbanking.infrastructure.api.dto.TransferResponseDto;
 import com.jetcheverry.interbanking.infrastructure.api.mapper.TransferDTOMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transfers")
+@Tag(name = "Transfer Controller", description = "Endpoints para gestionar transferencias")
 public class TransferController {
 
     private final TransferServicePort transferService;
@@ -25,6 +32,17 @@ public class TransferController {
         this.transferDtoMapper = transferDtoMapper;
     }
 
+    @Operation(
+            summary = "Registrar una nueva transferencia",
+            description = "Permite registrar una transferencia entre cuentas bancarias. " +
+                    "Si no se especifica `transferDate`, se asignará la fecha actual."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Transferencia registrada exitosamente",
+                    content = @Content(schema = @Schema(implementation = TransferResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Solicitud inválida"),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+    })
     @PostMapping
     public ResponseEntity<TransferResponseDto> registerTransfer(@Valid @RequestBody TransferRequestDto requestDto) {
         Transfer transfer = transferDtoMapper.toDomain(requestDto);
